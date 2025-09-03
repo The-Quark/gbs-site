b9141707fffc038acfd88c14b2e8bb62c8c097cf
Aijin <erbolat.j.2004@gmail.com>
2025-09-03 18:40:57 +0500

    feat():tailwind config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import './style.css'
+import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>Hello Vite!</h1>
+    <h1 class="text-primary font-bold underline">Hello Vite!</h1>
+    <p class="text-secondary">Secondary цвет</p>
+    <button class="bg-primary text-white px-4 py-2 rounded">
+     Кнопка
+    </button>
   </div>
-`
+`;
