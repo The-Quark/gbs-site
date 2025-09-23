1cc2ccc7a57a515aceb22a25dde3d147f65f389e
Aijin <erbolat.j.2004@gmail.com>
2025-09-23 16:40:26 +0500

    fi8x()

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'src/pages/about-us/about-us.html'),
-        devices: resolve(__dirname, 'src/pages/devices/devices.html'),
-        services: resolve(__dirname, 'src/pages/services/services.html'),
-        contacts: resolve(__dirname, 'src/pages/contacts/contacts.html'),
-        d_fend: resolve(__dirname, 'src/pages/d-fend/d-fend.html'),
-        privacy: resolve(__dirname, 'src/pages/privacy/privacy.html'),
+        about: resolve(__dirname, '/src/pages/about-us/about-us.html'),
+        devices: resolve(__dirname, '/src/pages/devices/devices.html'),
+        services: resolve(__dirname, '/src/pages/services/services.html'),
+        contacts: resolve(__dirname, '/src/pages/contacts/contacts.html'),
+        d_fend: resolve(__dirname, '/src/pages/d-fend/d-fend.html'),
+        privacy: resolve(__dirname, '/src/pages/privacy/privacy.html'),
       },
     },
   },
