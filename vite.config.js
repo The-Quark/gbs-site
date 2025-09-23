b5f24199dbf1893792ced5f9997dcab185584d00
Aijin <erbolat.j.2004@gmail.com>
2025-09-23 17:10:27 +0500

    fix():route

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,17 @@ import { resolve } from 'path';
 export default defineConfig({
   base: '/',
   plugins: [tailwindcss(), injectHTML()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/pages/about-us/about-us.html'),
+        devices: resolve(__dirname, 'src/pages/devices/devices.html'),
+        services: resolve(__dirname, 'src/pages/services/services.html'),
+        contacts: resolve(__dirname, 'src/pages/contacts/contacts.html'),
+        d_fend: resolve(__dirname, 'src/pages/d-fend/d-fend.html'),
+        privacy: resolve(__dirname, 'src/pages/privacy/privacy.html'),
+      },
+    },
+  },
 });
